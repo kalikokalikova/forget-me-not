1e4b96762d32b9484f16dcc0642d2b1bfcc39aff
Jennifer Stewart <jlgregg1@gmail.com>
2022-08-15 15:29:05 -0700

    add custom item

--- flask_app/controllers/items.py
+++ flask_app/controllers/items.py
@@ -10,3 +10,15 @@ from flask_app.models.item import Item
 #     return render_template('index.html', upcoming_trips=upcoming_trips)
 
 #route to add custom item to list
+@app.route("/add_item", methods = ["POST"])
+def add_item_to_db():
+    data = {
+        'name' : request.form['name'],
+        'weight' : request.form['weight'],
+        'is_packed' : request.form['is_packed'],
+        'categories_id' : request.form['categories_id'],
+        'lists_id' : request.form['lists_id']
+    }
+    list_id = data['lists_id']
+    Item.save(data)
+    return redirect(f'/view_trip/{list_id}')
